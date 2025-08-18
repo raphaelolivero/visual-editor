309580b6f5192d3c7635ec73c2d0b20e3cf75e81
RAPHAËL <6019511+raphaelolivero@users.noreply.github.com>
2025-08-18 03:25:55 -0700

    Add App.vue for visual editor demo

---
+++ packages/adapter-vue/demo/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Palette from './Palette.vue'
+import Canvas from './Canvas.vue'
+import Inspector from './Inspector.vue'
+import { createDoc, type Doc } from './doc'
+
+const doc = ref<Doc>(createDoc())
+const update = (d: Doc) => (doc.value = d)
+</script>
+
+<template>
+  <div class="min-h-screen p-4 bg-gray-100">
+    <div class="grid grid-cols-12 gap-4">
+      <aside class="col-span-3"><Palette /></aside>
+      <main class="col-span-6 h-[80vh]">
+        <Canvas :doc="doc" @update:doc="update" />
+      </main>
+      <aside class="col-span-3">
+        <Inspector :doc="doc" @update:doc="update" />
+      </aside>
+    </div>
+  </div>
+</template>
